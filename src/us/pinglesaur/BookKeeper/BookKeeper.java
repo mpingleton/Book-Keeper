4cfa8f75f7c8474c117f9083b69426dfaf0b6adf
Michael Pingleton <mnpingleton@gmail.com>
2023-09-27 11:24:40 -0400

    Figured out some BoxLayout and BorderLayout stuff.

--- src/us/pinglesaur/BookKeeper/BookKeeper.java
+++ src/us/pinglesaur/BookKeeper/BookKeeper.java
@@ -1,15 +1,35 @@
 package us.pinglesaur.BookKeeper;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class BookKeeper {
 
     public static void main(String[] args) {
         
+        // Search Tab.
         JPanel tabSearch = new JPanel();
-        JPanel tabCheckOut = new JPanel();
-        JPanel tabCheckIn = new JPanel();
         
+        // Check Out Tab.
+        JPanel tabCheckOut = new JPanel();
+        
+        // Check In Tab.
+        JButton barcodeEnter = new JButton("Enter");
+        JTextField barcodeInput = new JTextField();
+        
+        JPanel layoutCheckInBarcodeInput = new JPanel();
+        layoutCheckInBarcodeInput.setLayout(new BoxLayout(layoutCheckInBarcodeInput, BoxLayout.X_AXIS));
+        layoutCheckInBarcodeInput.add(barcodeInput);
+        layoutCheckInBarcodeInput.add(barcodeEnter);
+        
+        JList<String> listCheckIn = new JList<String>();
+        
+        JPanel tabCheckIn = new JPanel();
+        tabCheckIn.setLayout(new BorderLayout(5, 5));
+        tabCheckIn.add(layoutCheckInBarcodeInput, BorderLayout.NORTH);
+        tabCheckIn.add(listCheckIn, BorderLayout.CENTER);
+        
+        // Set up the tab selector and main window.
         JTabbedPane tabLayout = new JTabbedPane();
         tabLayout.add("Search", tabSearch);
         tabLayout.add("Check Out", tabCheckOut);
